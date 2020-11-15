6eaf04ba5fef4561b3454ff9a8d0f4a649b0987c
dutake <74233503+dutake@users.noreply.github.com>
2020-11-14 17:39:07 -0800

    Uploading
    lmao im trash at coding

---
+++ index.js
@@ -0,0 +1,57 @@
+const {	getModule,	getAllModules,	React,	constants,} = require("powercord/webpack");
+const ChannelContextMenu = getAllModules((m) =>	  m.default && m.default.displayName == "ChannelListVoiceChannelContextMenu",	false )[0];
+  const { getVoiceStates } = getModule(["getVoiceStates"], false);
+  const { inject, uninject } = require("powercord/injector");
+  const { patch } = getModule(["APIError", "patch"], false);
+  const Menu = getModule(["MenuGroup", "MenuItem"], false);
+  const { getChannel } = getModule(["getChannel"], false);
+  const { Plugin } = require("powercord/entities");
+  const { sleep } = require("powercord/util");
+  const getuser = require("powercord/webpack").getModule(["getCurrentUser"], false); // thanks to Oocrop for showing me how to get the user's perms
+  module.exports = class disconnectallvoicechat extends Plugin {
+	async startPlugin() {
+	  const can = (await getModule(["can", "canEveryone"])).can; 
+	  inject("disconect-all-vc", ChannelContextMenu, "default", (args, res) => {
+		let user = getuser.getCurrentUser(); //the user		
+		let channel = args[0].channel;
+		let channelmembers = this.getVoiceChannelMembers(channel.id);
+		if(channelmembers.length < 1) return res;
+  console.log(can(constants.Permissions.MOVE_MEMBERS, user, channel));
+		if (!can(constants.Permissions.MOVE_MEMBERS, user, channel)) return res;
+		console.log("DISCONNECT 2");
+		let item = React.createElement(Menu.MenuItem, {  //Found out how to add stuff to this kinda menu from https://github.com/Twizzer/move-all-vc
+		  action: async () => {
+			for (const member of channelmembers) {
+			  patch({
+				url: constants.Endpoints.GUILD_MEMBER(channel.guild_id, member), //Found out how to move members from https://github.com/Twizzer/move-all-vc
+				body: {
+				  channel_id: null, 
+				},
+			  });
+			  sleep(350); // gonna make this into something changable w settings later
+			}
+		  },
+		  id: "disconnect-all-vc",
+		  label: "Disconnect All",
+		});
+		let element = React.createElement(Menu.MenuGroup, null, item);
+		res.props.children.push(element); 
+		return res;
+	  });
+	  ChannelContextMenu.default.displayName =
+		"ChannelListVoiceChannelContextMenu"; 
+	}
+	pluginWillUnload() {
+	  uninject("disconect-all-vc");
+	}
+	getVoiceUserIds(guild, channel) {
+	  return Object.values(getVoiceStates(guild))
+		.filter((c) => c.channelId == channel)
+		.map((a) => a.userId);
+	} 
+	getVoiceChannelMembers(id) {
+	  let channel = getChannel(id);
+	  return this.getVoiceUserIds(channel.guild_id, channel.id);
+	}
+  };
+  
